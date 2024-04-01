d05f2c797e2248a0b56ab0c24eccd4340f933128
ReyanGhanim <reyanghanim@outlook.com>
2024-04-01 18:04:41 -0500

    app with slight design

--- OTW-Watch-iOS-App/App.js
+++ OTW-Watch-iOS-App/App.js
@@ -4,7 +4,29 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Reyan Ghanim</Text>
+
+      <View style={{ flexDirection: 'row'}}>
+        <View style = {styles.valueContainer}>
+          <Text style = {styles.label}>Steps</Text>
+          <Text style = {styles.value}>5083</Text>
+        </View>
+
+        <View style = {styles.valueContainer}>
+          <Text style = {styles.label}>Distance</Text>
+          <Text style = {styles.value}>1.65 miles</Text>
+        </View>
+
+        
+      </View>
+
+      
+      <View style = {styles.valueContainer}>
+        <Text style = {styles.label}>Flights Climbed</Text>
+        <Text style = {styles.value}>10 flights</Text>
+      </View>
+      
+
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +35,21 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    backgroundColor: 'black',
     justifyContent: 'center',
+    padding: 12,
+  },
+  valueContainer: {
+    marginRight: 50,
+    marginVertical: 10,
+  },
+  label: {
+    color: "white",
+    fontSize: 20,
+  },
+  value: {
+    fontSize: 35,
+    color: '#AFB3BE',
+    fontWeight: '500',
   },
 });
