ca922cd49cd45e9dc3f40711161f462249e23f58
SamuelJacob146 <samuel.t.jacob888@gmail.com>
2024-05-01 15:08:28 -0500

    final update

--- OTW-Watch-iOS-App/App.tsx
+++ OTW-Watch-iOS-App/App.tsx
@@ -1,16 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import React, {useState } from 'react';
-import { Button, StyleSheet, Text, View, Alert, Image } from 'react-native';
+import { Button, StyleSheet, Text, View, Alert, Image, TouchableOpacity } from 'react-native';
 import AppleHealthKit, {HealthInputOptions, HealthKitPermissions} from 'react-native-health';
 import {useEffect} from 'react';
 import {db, auth} from './firebaseConfig';
 import {doc, updateDoc, setDoc, addDoc, deleteDoc, collection} from "firebase/firestore";
 import fitlyLogo from './fitly_logo.png';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 const permissions: HealthKitPermissions = {
   permissions: {
-    read: [AppleHealthKit.Constants.Permissions.Steps, AppleHealthKit.Constants.Permissions.FlightsClimbed
-    , AppleHealthKit.Constants.Permissions.SleepAnalysis, AppleHealthKit.Constants.Permissions.HeartRate],
+    read: [AppleHealthKit.Constants.Permissions.Steps,AppleHealthKit.Constants.Permissions.SleepAnalysis, AppleHealthKit.Constants.Permissions.HeartRate, 
+      AppleHealthKit.Constants.Permissions.ActiveEnergyBurned],
     write: [],
   },
 };
@@ -52,34 +53,87 @@ export default function App() {
     if (err) {
       console.log("Error getting the steps");
     }
+
+    
     console.log(results);
     setSteps(results.value);
   });
 
-    AppleHealthKit.getFlightsClimbed(options, (err, results) => {
-    if (err) {
-      console.log("Error getting the steps");
-    }
-    console.log(results);
-    setFlights(results.value);
-  });
 
   }, [hasPermissions]);
   
+  useEffect(() => {
+    if (!hasPermissions) {
+      return;
+    }
+
+    const options: HealthInputOptions = {
+      date: new Date(2023, 4, 15).toISOString(),
+      includeManuallyAdded: false,
+    };
+
+    AppleHealthKit.getHeartRateSamples(options, (err, results) => {
+    if (err) {
+      console.log("Error getting the heart rate");
+    }
+
+    
+    console.log(results);
+  });
+}, [hasPermissions]);
+
+useEffect(() => {
+  if (!hasPermissions) {
+    return;
+  }
+
+  const options: HealthInputOptions = {
+    date: new Date(2023, 4, 15).toISOString(),
+    includeManuallyAdded: false,
+  };
+
+  AppleHealthKit.getSleepSamples(options, (err, results) => {
+  if (err) {
+    console.log("Error getting the sleep samples");
+  }
+
+  
+  console.log(results);
+});
+}, [hasPermissions]);
+
+useEffect(() => {
+  if (!hasPermissions) {
+    return;
+  }
+
+  const options: HealthInputOptions = {
+    date: new Date(2023, 4, 15).toISOString(),
+    includeManuallyAdded: false,
+  };
+
+  AppleHealthKit.getActiveEnergyBurned(options, (err, results) => {
+  if (err) {
+    console.log("Error getting the energy burned");
+  }
+
+  
+  console.log(results);
+});
+}, [hasPermissions]);
 
   const sendToTrainer = async () => {
     try {
       const healthData = {
         steps: 5083,
-        weeklyAverageSteps: 6247,
-        flightsClimbed: 10,
+        weeklyAverageSteps: 5247,
+        heartRate: "68 bpm",
         hoursSlept: "7hr 35min",
         activeCaloriesBurned: "199 cal"
 
-        // Add other health data here
       };
 
-      await addDoc(collection(db, 'healthData'), healthData);
+      await setDoc(doc(db, 'healthData', 'user_one'), healthData);
       Alert.alert("Data Successfully Sent", "Your health data has been sent to the trainer successfully!");
       console.log("Health data sent to trainer successfully!");
     } catch (error) {
@@ -94,8 +148,8 @@ export default function App() {
 
       <View style={styles.container}>
         <Value label="Today's Steps" value={5083} />
-        <Value label="Weekly Average Steps" value = {6247} />
-        <Value label="Flights Climbed" value={"7 flights"}/>
+        <Value label="Weekly Average Steps" value = {5247} />
+        <Value label="Heart Rate" value={"68 bpm"}/>
         <Value label="Hours Slept" value={"7hr 35min"}/>
         <Value label="Active Calories Burned" value={"199 cal"}/>
       </View>
@@ -104,19 +158,40 @@ export default function App() {
       <Button 
           onPress = {sendToTrainer}
           title = "Send To Trainer"
-          color = "black"
+          color = "white"
           />
+          
+      </View>
+
+      <View style={styles.bottomDashboard}>
+      <View style={styles.bottomButton}>
+        <Icon name="cog" size={45} color="white" />
+          
+
+      </View>
+        <View style={styles.bottomButton}>
+        <Icon name="home" size={45} color="white" />
+        </View>
+
+        <View style={styles.bottomButton}>
+        <Icon name="line-chart" size={45} color="white" />
+        </View>
+
+        <View style={styles.bottomButton}>
+        <Icon name="user" size={45} color="white" />
+        </View>
       </View>
 
       <StatusBar style="auto" />
     </View>
+
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'black',
+    backgroundColor: 'white',
     justifyContent: 'center',
     padding: 12,
   },
@@ -125,25 +200,54 @@ const styles = StyleSheet.create({
     marginVertical: 10,
   },
   label: {
-    color: "white",
+    color: "black",
     fontSize: 20,
   },
   value: {
     fontSize: 35,
-    color: '#AFB3BE',
+    color: 'black',
     fontWeight: '500',
   },
   btn: {
-    backgroundColor: "white",
+    backgroundColor: "#088A20",
     fontSize: 36,
-    marginBottom: 200,
-    marginTop: 70,
-    borderRadius: 10
+    marginBottom: 100,
+    marginTop: 80,
+    borderRadius: 10,
+    fontWeight: 'bold'
   },
   logo: {
     width: 200,
     height: 100,
-    marginBottom: 20,
+    marginBottom: 30,
     marginTop: 30
-  }
+  },
+  bottomDashboard: {
+    backgroundColor: "#088A20",
+    height: 120,
+    width: 410,
+    marginLeft: -20,
+    marginBottom: -20,
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+
+  },
+  bottomButton: {
+    backgroundColor: '#088A20',
+    padding: 0,
+    borderRadius: 10,
+    paddingHorizontal: 0,
+
+
+  },
+  bottomButtonText: {
+    color: 'white',
+    fontSize: 26,
+    textAlign: 'center',
+    marginLeft: -10,
+    marginBottom: 10,
+    fontWeight: 'bold'
+    
+  },
 });
